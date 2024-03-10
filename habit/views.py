e4d305558c6fc51a3bc815860c33b7d109396e8a
Andrey Potapov <aapot@ya.ru>
2024-03-10 07:45:24 +0300

    habit paginator

--- habit/views.py
+++ habit/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from habit.models import Habit
+from habit.paginators import HabitPaginator
 from habit.serializers import HabitSerializer
 
 
@@ -7,6 +8,7 @@ class HabitListCreate(generics.ListCreateAPIView):
     """Create and Listview привычек"""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
 
     def perform_create(self, serializer):
         """Добавление поля пользователя"""
